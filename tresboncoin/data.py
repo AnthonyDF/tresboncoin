5609cbefc8f05dbd3ac50d753d750d43d7d8e818
VictorBonnet <victor.bonnet.mg@gmail.com>
2021-06-12 18:31:56 +0200

    saturday12

--- tresboncoin/data.py
+++ tresboncoin/data.py
@@ -13,16 +13,14 @@ def get_data():
 
 def clean_data(df):
     ''' return clean dataframe '''
-    df = df.drop(["Name",
-                  "PassengerId",
-                  "Ticket",
-                  "Embarked",
-                  "Parch",
-                  "SibSp",
-                  "Cabin"], axis=1)
+    df = df[~df["brand_db"].isnull()]
+    df = df[~df["model_db"].isnull()]
+    df = df[~df["type_db"].isnull()]
+    df = df[~df["category_db"].isnull()]
     return df
 
 
 if __name__ == '__main__':
     df_train = get_data()
+    df_train = clean_data(df_train)
     print("Train dataframe loaded. Shape:" + str(df_train.shape))
