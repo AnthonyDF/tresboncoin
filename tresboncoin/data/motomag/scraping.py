16e9b1c64813c2eef51741bb8b922bfbc6921a76
VictorBonnet <victor.bonnet.mg@gmail.com>
2021-07-14 11:28:48 +0200

    motomag fix

--- tresboncoin/data/motomag/scraping.py
+++ tresboncoin/data/motomag/scraping.py
@@ -1,19 +1,16 @@
 from tresboncoin.data.motomag.scraping_pages import scraping_pages
 from tresboncoin.data.motomag.scraping_annonces import scraping_annonces
-from tresboncoin.data.motomag.scraping_to_dataframe import scraping_to_dataframe
 
 
 def scraping():
     '''
     function to:
     1 - scraping pages
-    2 - scrapping annonces
-    3 - tranform to dataframe
+    2 - scrapping and saving new annonces
     '''
 
     scraping_pages()
     scraping_annonces()
-    scraping_to_dataframe()
 
 
 if __name__ == "__main__":
